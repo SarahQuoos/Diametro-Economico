5599ea68a08c903031b0479ac4f7b4a601d8a126
SarahQuoos <sarahquoos@alunos.utfpr.edu.br>
2025-04-08 18:13:48 -0300

    Update repeat.py
    Major update

--- repeat.py
+++ repeat.py
@@ -1,11 +1,10 @@
 import streamlit as st
-from st_aggrid import AgGrid
+#from st_aggrid import AgGrid
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-#Configuração de Página
+#Streamlit page config
 st.set_page_config(
     page_title="Dimensionamento Diâmetro Econômico",
     layout="wide",
@@ -15,174 +14,95 @@ st.set_page_config(
         'About': "# Desenvolvido por Sarah Quoos e Edgar Silva. UTFPR, Curitiba, PR, 2025"}
     )
 
-#Título
+#Page Title
 st.title("Dimensionamento de Diâmetro Econômico de Adutoras em Estações Elevatórias de Água")
 
-#Dados de Entrada
-with st.sidebar:
-    st.title("Dados de entrada")
-    Q = st.number_input('Vazão requerida em m³/h', value=0)
-    L = st.number_input('Comprimento da adutora em metros:', value=0)
-    Namin = st.number_input('Cota do nível de água mínimo no poço de sucção do bombeamento em metros:', value=0)
-    Namax = st.number_input('Cota do nível de água máximo no reservatório elevado em metros:', value=0)  
-    M = st.selectbox("Material da tubulação?",("select","Ferro Fundido", "PVC", "PRVF"),)
+#Access database rotine
+def Accessing_database():
+    sheet_material = pd.read_excel('Banco de Dados.xlsx', sheet_name=material)
 
-#Acesso ao banco de dados
-def Banco():
-    sheet = pd.read_excel('Banco de Dados.xlsx', sheet_name=M)
-    #Diâmetro interno
-    diaux = sheet['Diâmetro interno'].tolist()
-    di = np.array(diaux)
-    #Diâmetro nominal
-    dnaux = sheet['Diâmetro nominal'].tolist()
-    dn = np.array(dnaux)
-    #Rugosidade
-    e = sheet.loc[0, 'Rugosidade [mm]']
-    return di, dn, e
+    inner_diameter_aux = sheet_material['Diâmetro interno'].tolist()
+    inner_diameter = np.array(inner_diameter_aux)
+
+    nominal_diameter_aux = sheet_material['Diâmetro nominal'].tolist()
+    nominal_diameter = np.array(nominal_diameter_aux)
+    
+    roughness = sheet_material.loc[0, 'Rugosidade [mm]']
+    return inner_diameter, nominal_diameter, roughness
 
 
-#Cálculos
-def Calculos():
+#Main rotine
+def Main():
     
     #Area
     pi = np.pi
-    A = (pi * ((di/1000)**2))/4 
+    area = (pi * ((inner_diameter/1000)**2))/4 
                     
     #Velocidade media
-    q = Q/3600
-    U = q/A
+    speed = (flow/3600)/area
                     
     #Dados da agua
-    p = 998 #massa específica [kg/m³]
-    u = 0.001 #viscosidade dinamica [m²/s]
+    water_specific_mass = 998 #massa específica [kg/m³]
+    water_dynamic_viscosity = 0.001 #viscosidade dinamica [m²/s]
                                       
     #Número de Reynolds
-    Re =  (p * (di/1000) * U)/u
+    reynolds = (water_specific_mass*(inner_diameter/1000)*speed)/water_dynamic_viscosity
                         
     #Fator de atrito
-    f = (-1.8*np.log10(((e/di)/3.7)**1.11 + (6.9/Re)))**-2
+    f = (-1.8*np.log10(((roughness/inner_diameter)/3.7)**1.11 + (6.9/reynolds)))**-2
                         
     #Perdas de carga distribuída
-    g = 9.81 #gravidade [m²/s]
-    hf = (f * L* U**2)/((di/1000) * 2 * g)
+    gravity = 9.81 #gravidade [m²/s]
+    major_pressure_loss = (f*length*speed**2)/((inner_diameter/1000)*2*gravity)
                     
     #Perdas de carga localizada
-    hl = hf * 0.1
+    minor_pressure_loss = major_pressure_loss*0.1
                     
     #Perda de carga total
-    ht = hf + hl
+    total_pressure_losses = major_pressure_loss + minor_pressure_loss
                     
+    #Cota do nível de água
+    water_level = max_water_level - min_water_level
+    
     #Altura manométrica
-    hg = Namax - Namin
-    hm = hg + ht
+    manometric_height = total_pressure_losses + water_level
                     
     #Rendimento global
-    ng = 0.70
+    global_efficiency = 0.70
                     
     #Potência requerida pela estação elevatória
-    W = (9.8 * q * hm)/ng
+    required_power = (9.8*(flow/3600)*manometric_height)/global_efficiency
                            
-    #Volume de escavação 
-    #Pf = Edgar
-    #Ve = (di + 2 * L + m * (di + Pf)) * (di + Pf) 
-                    
-    #Volume para aterro da vala
-    #Va = Ve - ((pi * di**2)/4)
-                    
-    #Bota-fora
-    #Vbf = 1.3 * ((pi * d**2)/4)
-                    
-    #Área de reposição pavimento
-    #m = Inclinação da vala, Edgar
-    #Ar = 2 * m * (di + Pf) + 2 * L + d
-                    
-    #Custo de montagem
-    #Md = (Ve * Pe) + (Va * Pa) + (Vbf * Pbf) + (Ar * Pr)
-                    
-    #Custo de Implementação
-    #Pd = Considerar só o preço do tubo
-    #Cd = (Pd + Md) * L
-                    
-    #Coeficiente de atualização de energia
-    #i= 0.12
-    #e = taxa de aumento de energia, Edgar
-    #n = 30 
-    #Fa = (((1 + e)**n - (1 + i)**n)/((1 + e) - (1 + i)))*(1/((1 + i)**n))
-                    
-    #Custo total
-    #Nb = número de horas, Edgar
-    #p = 
-    #Ct = Cd * L * ((9.81 * Q * hm * nb * p * Fa)/ng)
-    
-    #Botão mostrar resultados em forma de tabela
-    if st.checkbox("Tabela Cálculos"):
-            data_tab = {'Diametro interno': di, 'Area': A, 'Velocidade': U, 'Reynolds': Re, 'Fator de atrito': f,
-                            'Perda de carga distribuída': hf, 'Perda de carga localizada': hl, 
-                            'Perda de carga total': ht, 'Altura manométrica': hm, 'Potência requerida': W}
-            tab = pd.DataFrame(data_tab)
-            st.table(tab)
-            
-    #return Md, Cd, Pd, Ct
-    return di, hf, hl, ht, W
-    
-            
-#Encontrando Valores Econômicos       
-def Economico():
-    
-    dnmin = min(dn) 
-    tam = len(dn)
+          
+    #Encontrando Valores Econômicos       
+    min_nominal_diameter = min(nominal_diameter) 
+    tam = len(nominal_diameter)
         
     i = 0
         
     while i <= tam:
-        d1 = dn[i]
-        if d1 == dnmin:
-            de = dnmin
-            We = W[i]
-            hfe = hf[i]
-            hle = hl[i]
-            hte = ht[i] 
+        d1 = nominal_diameter[i]
+        if d1 == min_nominal_diameter:
+            extenal_diameter = min_nominal_diameter
+            We = required_power[i]
+            hfe = major_pressure_loss[i]
+            hle = minor_pressure_loss[i]
+            hte = total_pressure_losses[i] 
             break
         else:
             i = i + 1
 
-    #Ctmin = min(Ct)
-    #tam = len(Ct)
-    
-    #i = 0
-        
-    #while i <= tam:
-        #Ct1 = Ct[i]
-        #if Ct1 == Ctmin:
-            #Cte = Ctmin
-            #st.write(Cte)
-            #Mde = Md[i]
-            #st.write(Mde)
-            #Cde = Cd[i]
-            #st.write(Cde)
-            #Pde = Pd[i] 
-            #st.write(Pde)
-            #de = d[i]
-            #st.write(de)
-        #else:
-           #i = i + 1 
-
-    #return Cte, Mde, Cde, Pde, de    
-    return hfe, hle, hte, de, We
-
-#Gráficos e Resultados
-def Resultado():
-    
-    graf_1, graf_2 = st.columns(2)
+    #Gráficos e Resultados
+    line_chart, pizza_chart = st.columns(2)
     
     #Gráfico Linha
-    with graf_1:
+    with line_chart:
         st.markdown("### Potência Requerida[W] x Diâmetro Nominal[mm]")
-        chart_data = {'Potencia Requerida': W, 'Diâmetro nominal': dn}
+        chart_data = {'Potencia Requerida': required_power, 'Diâmetro nominal': nominal_diameter}
         st.line_chart(chart_data, x="Diâmetro nominal", y="Potencia Requerida",height=500)
     
     #Gráfico Pizza
-    with graf_2:
+    with pizza_chart:
         st.markdown("### Relação entre Perdas de Carga")
         labels = 'Perdas de carga distribuída', 'Perdas de carga localizada', 'Perda total'
         sizes = [hfe, hle, hte]
@@ -190,229 +110,49 @@ def Resultado():
         ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90, normalize=True,)
         st.pyplot(fig1)
     
-    #Tabela Resultados
+    #Results table
     st.markdown("### Resultados para o menor diâmetro")
     
     tab1, tab2, tab3, tab4, tab5 = st.columns(5)
-    tab1.metric(label="Diâmetro Econômico", value=de,)
-    tab2.metric(label="Potência Requerida", value=f" {round(We,2)} ",)
+    tab1.metric(label="Diâmetro Econômico", value=extenal_diameter,)
+    tab2.metric(label="Potência Requerida", value=f"{round(We,2)} ",)
     tab3.metric(label="Perdas de carga distribuída", value=f" {round(hfe,2)} ",)
     tab4.metric(label="Perdas de carga localizada", value=f" {round(hle,2)} ",)
     tab5.metric(label="Perdas de carga total", value=f" {round(hte,2)} ",)
     
-#Botões    
-def Botões(): 
-    
-    bot_1, bot_2 = st.columns(2)
-
-    with bot_1:
-        if st.checkbox("Source Code"):
-            code = '''
-import streamlit as st
-from st_aggrid import AgGrid
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-
-
-#Configuração de Página
-st.set_page_config(
-    page_title="Dimensionamento Diâmetro Econômico",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# Desenvolvido por Sarah Quoos e Edgar Silva. UTFPR, Curitiba, PR, 2025"}
-    )
-
-#Título
-st.title("Dimensionamento de Diâmetro Econômico de Adutoras em Estações Elevatórias de Água")
-
-#Dados de Entrada
+    #Botões/Opções de visualização           
+    if st.checkbox("Mostrar tabela de cálculos"):
+        st.write(st.session_state.test)
+        data_table = {'Diametro interno': inner_diameter, 'Area': area, 'Velocidade': speed, 'Reynolds': reynolds, 'Fator de atrito': f,
+                      'Perda de carga distribuída': major_pressure_loss, 'Perda de carga localizada': minor_pressure_loss, 
+                      'Perda de carga total': total_pressure_losses, 'Altura manométrica': manometric_height, 'Potência requerida': required_power}
+        calculations_table = pd.DataFrame(data_table)
+        st.table(calculations_table)
+                          
+#Loop principal
+submit_button_check = 0
 with st.sidebar:
     st.title("Dados de entrada")
-    Q = st.number_input('Vazão requerida em m³/h', value=0)
-    L = st.number_input('Comprimento da adutora em metros:', value=0)
-    Namin = st.number_input('Cota do nível de água mínimo no poço de sucção do bombeamento em metros:', value=0)
-    Namax = st.number_input('Cota do nível de água máximo no reservatório elevado em metros:', value=0)  
-    M = st.selectbox("Material da tubulação?",("select","Ferro Fundido", "PVC", "PRVF"),)
-
-#Acesso ao banco de dados
-def Banco():
-    sheet = pd.read_excel('Banco de Dados.xlsx', sheet_name=M)
-    #Diâmetro interno
-    diaux = sheet['Diâmetro interno'].tolist()
-    di = np.array(diaux)
-    #Diâmetro nominal
-    dnaux = sheet['Diâmetro nominal'].tolist()
-    dn = np.array(dnaux)
-    #Rugosidade
-    e = sheet.loc[0, 'Rugosidade [mm]']
-    return di, dn, e
-
-#Cálculos
-def Calculos():
-    
-    #Area
-    pi = np.pi
-    A = (pi * ((di/1000)**2))/4 
-                    
-    #Velocidade media
-    q = Q/3600
-    U = q/A
-                    
-    #Dados da agua
-    p = 998 #massa específica [kg/m³]
-    u = 0.001 #viscosidade dinamica [m²/s]
-                                      
-    #Número de Reynolds
-    Re =  (p * (di/1000) * U)/u
-                        
-    #Fator de atrito
-    f = (-1.8*np.log10(((e/di)/3.7)**1.11 + (6.9/Re)))**-2
-                        
-    #Perdas de carga distribuída
-    g = 9.81 #gravidade [m²/s]
-    hf = (f * L* U**2)/((di/1000) * 2 * g)
-                    
-    #Perdas de carga localizada
-    hl = hf * 0.1
-                    
-    #Perda de carga total
-    ht = hf + hl
-                    
-    #Altura manométrica
-    hg = Namax - Namin
-    hm = hg + ht
-                    
-    #Rendimento global
-    ng = 0.70
-                    
-    #Potência requerida pela estação elevatória
-    W = (9.8 * q * hm)/ng
-                           
-    #Botão mostrar resultados em forma de tabela
-    if st.checkbox("Tabela Cálculos"):
-            data_tab = {'Diametro interno': di, 'Area': A, 'Velocidade': U, 'Reynolds': Re, 'Fator de atrito': f,
-                            'Perda de carga distribuída': hf, 'Perda de carga localizada': hl, 
-                            'Perda de carga total': ht, 'Altura manométrica': hm, 'Potência requerida': W}
-            tab = pd.DataFrame(data_tab)
-            st.table(tab)
+    with st.form(key='Dados de entrada'):
+        flow = st.number_input('Vazão requerida em m³/h')
+        length = st.number_input('Comprimento da adutora em metros:')
+        min_water_level = st.number_input('Cota do nível de água mínimo no poço de sucção do bombeamento em metros:')
+        max_water_level = st.number_input('Cota do nível de água máximo no reservatório elevado em metros:')  
+        material = st.selectbox("Material da tubulação?",("Select","Ferro Fundido", "PVC", "PRVF"),)
             
-    #return Md, Cd, Pd, Ct
-    return di, hf, hl, ht, W
-    
-            
-#Encontrando Valores Econômicos       
-def Economico():
-    
-    dnmin = min(dn) 
-    tam = len(dn)
+        button_submit, button_reset = st.columns(2)
         
-    i = 0
-        
-    while i <= tam:
-        d1 = dn[i]
-        if d1 == dnmin:
-            de = dnmin
-            We = W[i]
-            hfe = hf[i]
-            hle = hl[i]
-            hte = ht[i] 
-            break
-        else:
-            i = i + 1
+        with button_submit:
+            if st.form_submit_button("Submit"):
+                if (flow == 0) or (length == 0) or (min_water_level == 0) or (max_water_level == 0) or (material == "Select"): 
+                    st.write("Preencha todas as informações necessárias!")
+                else:
+                    submit_button_check = 1
+        with button_reset:
+            if st.form_submit_button("Reset"):
+                submit_button_check = 0
+                st.rerun()
 
-#Gráficos e Resultados
-def Resultado():
-    
-    graf_1, graf_2 = st.columns(2)
-    
-    #Gráfico Linha
-    with graf_1:
-        st.markdown("### Potência Requerida x Diâmetro")
-        chart_data = {'Potencia Requerida': W, 'Diâmetro nominal': dn}
-        st.line_chart(chart_data, x="Diâmetro nominal", y="Potencia Requerida",height=500)
-    
-    #Gráfico Pizza
-    with graf_2:
-        st.markdown("### Perdas de Carga")
-        labels = 'Perdas de carga distribuída', 'Perdas de carga localizada', 'Perda total'
-        sizes = [hfe, hle, hte]
-        fig1, ax1 = plt.subplots()
-        ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90, normalize=True,)
-        st.pyplot(fig1)
-    
-    #Tabela Resultados
-    st.markdown("### Resultados para o menor diâmetro")
-    
-    tab1, tab2, tab3, tab4, tab5 = st.columns(5)
-    tab1.metric(label="Diâmetro Econômico", value=de,)
-    tab2.metric(label="Potência Requerida", value=f" {round(We,4)} ",)
-    tab3.metric(label="Perdas de carga distribuída", value=f" {round(hfe,4)} ",)
-    tab4.metric(label="Perdas de carga localizada", value=f" {round(hle,4)} ",)
-    tab5.metric(label="Perdas de carga total", value=f" {round(hte,4)} ",)
-    
-#Botões    
-def Botões(): 
-    
-    bot_1, bot_2 = st.columns(2)
-
-    with bot_1:
-        if st.checkbox("Source Code"):
-            st.code(code, language="python")
-    
-    with bot_2:
-        if st.checkbox("Banco de dados"):    
-            file = pd.read_excel('Banco de Dados.xlsx', sheet_name='Dados')
-            AgGrid(file)
-
-#Rotina do Programa
-if M == 'Ferro Fundido':
-    di, dn, e = Banco()
-    di, hf, hl, ht, W = Calculos()
-    hfe, hle, hte, de, We = Economico()
-    Resultado()
-    Botões()
-    
-elif M == 'PVC':
-    di, dn, e = Banco()
-    di, hf, hl, ht, W = Calculos()
-    hfe, hle, hte, de, We = Economico()
-    Resultado()
-    Botões()
-        
-elif M == 'PRVF':
-    di, dn, e = Banco()
-    di, hf, hl, ht, W = Calculos()
-    hfe, hle, hte, de, We = Economico()
-    Resultado()
-    Botões()''' 
-            st.code(code, language="python")
-    
-    with bot_2:
-        if st.checkbox("Banco de dados"):    
-            file = pd.read_excel('Banco de Dados.xlsx', sheet_name='Dados')
-            AgGrid(file)
-
-#Rotina do Programa
-if M == 'Ferro Fundido':
-    di, dn, e = Banco()
-    di, hf, hl, ht, W = Calculos()
-    hfe, hle, hte, de, We = Economico()
-    Resultado()
-    Botões()
-    
-elif M == 'PVC':
-    di, dn, e = Banco()
-    di, hf, hl, ht, W = Calculos()
-    hfe, hle, hte, de, We = Economico()
-    Resultado()
-    Botões()
-        
-elif M == 'PRVF':
-    di, dn, e = Banco()
-    di, hf, hl, ht, W = Calculos()
-    hfe, hle, hte, de, We = Economico()
-    Resultado()
-    Botões()
+if submit_button_check == 1:
+    inner_diameter, nominal_diameter, roughness = Accessing_database()
+    Main()
