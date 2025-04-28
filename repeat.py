235d0d602d51ef4a49d0e008fe4b3619197d5f3d
SarahQuoos <sarahquoos@alunos.utfpr.edu.br>
2025-04-28 12:53:15 -0300

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -122,9 +122,9 @@ def Main():
     st.line_chart(chart_data, x="Diâmetro nominal", y="Custo Total",height=500)
 
     #Line Chart Try
-    #chart_data1 = {'Custo Total': total_cost_meter, 'Diâmetro nominal': nominal_diameter}
-    #fig = px.line(chart_data1, x="Diâmetro nominal", y="Custo Total", title='Custo Total (R$/m) x Diâmetro Nominal(mm')
-    #fig.show()
+    chart_data1 = {'Custo Total': total_cost_meter, 'Diâmetro nominal': nominal_diameter}
+    fig = px.line(chart_data1, x="Diâmetro nominal", y="Custo Total", title='Custo Total (R$/m) x Diâmetro Nominal(mm')
+    fig
     
     #Results table
     st.markdown("### Resultado")
@@ -187,8 +187,3 @@ with st.sidebar:
 
 if submit_button_check == 1:
     Main()
-    
-st.markdown("### AAAAA") 
-t = np.linspace(0, 2*np.pi, 100)
-fig = px.line(x=t, y=np.cos(t), labels={'x':'t', 'y':'cos(t)'})
-fig
