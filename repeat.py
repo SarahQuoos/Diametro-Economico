0fff83112a4d4fd9da2f8acfc912bbc94a7f004e
SarahQuoos <sarahquoos@alunos.utfpr.edu.br>
2025-04-29 17:27:39 -0300

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -145,7 +145,7 @@ def Main():
     
     calculations_table = pd.DataFrame(data_table)
     rounded_table = calculations_table.round(2).copy()
-    rounded_table = rounded_table.astype(str).apply(lambda x: x.str.rstrip('0').str.rstrip('.'))
+    #rounded_table = rounded_table.astype(str).apply(lambda x: x.str.rstrip('0').str.rstrip('.'))
     #rounded_table = calculations_table.apply(lambda x: "{:,.2f}".format(x['Value']), axis=1)
     
     #Calculations table view
