640d5a281dc55b84f173fc54d5bf6bdd169dbb07
SarahQuoos <sarahquoos@alunos.utfpr.edu.br>
2025-04-29 17:14:16 -0300

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -144,9 +144,12 @@ def Main():
                   'Custo de Operação': operation_cost, 'Custo Total do Projeto': total_cost, 'Custo Total por Metro': total_cost_meter}
     
     calculations_table = pd.DataFrame(data_table)
+    
+    rounded_aux = calculations_table.head().style.format("{:,.2f}")
+    rounded_table = rounded_aux.copy()
+    
     #rounded_table = calculations_table.round(2).copy()
     #rounded_table = rounded_table.astype(str).apply(lambda x: x.str.rstrip('0').str.rstrip('.'))
-    rounded_table = calculations_table.head().style.format("{:,.2f}")
     
     #Calculations table view
     with st.expander("Visualizar Tabela de Resultados"):
