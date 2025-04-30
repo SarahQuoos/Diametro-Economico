a8c28d6018b0312a8e8bad6840b2f4dec590052d
SarahQuoos <sarahquoos@alunos.utfpr.edu.br>
2025-04-30 20:05:56 -0300

    Update Diametro_Economico.py

--- Diametro_Economico.py
+++ Diametro_Economico.py
@@ -144,7 +144,7 @@ def Main():
                   'Custo de Operação': operation_cost, 'Custo Total do Projeto': total_cost, 'Custo Total por Metro': total_cost_meter}
     
     calculations_table = pd.DataFrame(data_table)
-    calculations_table['Custo Total por Metro'] = calculations_table['Custo Total por Metro'].apply(lambda x: "{:,.2f}".format(x))
+    calculations_table = calculations_table.apply(lambda x: "{:,.2f}".format(x))
     
     #rounded_table = calculations_table.round(2).copy()
     #rounded_table = rounded_table.astype(str).apply(lambda x: x.str.rstrip('0').str.rstrip('.'))
