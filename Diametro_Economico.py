9ef57b76b467eb8dc5c62f5061520b7c3049e289
SarahQuoos <sarahquoos@alunos.utfpr.edu.br>
2025-05-02 11:50:22 -0300

    Update Diametro_Economico.py

--- Diametro_Economico.py
+++ Diametro_Economico.py
@@ -153,12 +153,12 @@ def Main():
     #values = values.apply(lambda x: "{:,.2f}".format(x))
     #rounded_table = calculations_table.round(2)
     #rounded_table = rounded_table.astype(str).apply(lambda x: x.str.rstrip('0').str.rstrip('.'))
-    rounded_table = calculations_table.apply(lambda x: "{:,.2f}".format(x['Value']), axis=1)
+    #rounded_table = calculations_table.apply(lambda x: "{:,.2f}".format(x['Value']), axis=1)
     
     #Calculations table view
     with st.expander("Visualizar Tabela de Resultados"):
         #st.dataframe(calculations_table.style.applymap(lambda _: "background-color: LightSkyBlue;", subset=([aux], slice(None))))
-        st.dataframe(rounded_table)
+        st.dataframe(calculations_table.style.applymap(lambda x: "{:,.2f}".format(x['Value']), axis=1)
               
 #Main Loop
 submit_button_check = 0
