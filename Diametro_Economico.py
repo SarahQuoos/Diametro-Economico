6f148ca481283c04ef60a2a300eda3c690247ff2
SarahQuoos <sarahquoos@alunos.utfpr.edu.br>
2025-05-02 11:47:44 -0300

    Update Diametro_Economico.py

--- Diametro_Economico.py
+++ Diametro_Economico.py
@@ -145,7 +145,8 @@ def Main():
     
     calculations_table = pd.DataFrame(data_table)
     #View values format
-    calculations_table['Coeficiente de Atualização da Energia'] = calculations_table['Coeficiente de Atualização da Energia'].apply(lambda x: "{:.2f}".format(x))
+    values = calculations_table[["Diâmetro Interno", "Coeficiente de Atualização da Energia"]]
+    calculations_table = values.apply(lambda x: "{:.2f}".format(x))
     calculations_table['Custo de Operação'] = calculations_table['Custo de Operação'].apply(lambda x: "{:,.2f}".format(x))
     calculations_table['Custo Total do Projeto'] = calculations_table['Custo Total do Projeto'].apply(lambda x: "{:,.2f}".format(x))
     calculations_table['Custo Total por Metro'] = calculations_table['Custo Total por Metro'].apply(lambda x: "{:,.2f}".format(x))
