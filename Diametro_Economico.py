e7dc2937dbcad77510d433df7898fbe3c9e1e835
SarahQuoos <sarahquoos@alunos.utfpr.edu.br>
2025-05-05 16:19:30 -0300

    Update Diametro_Economico.py

--- Diametro_Economico.py
+++ Diametro_Economico.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import openpyxl
-import io
 
 #Streamlit page config
 st.set_page_config(
@@ -147,13 +146,6 @@ def Main():
     #View values format
     with st.expander("Visualizar Tabela de Resultados"):
         st.dataframe(calculations_table.style.format(precision=2,decimal=",",thousands=".").applymap(lambda _: "background-color: LightSkyBlue;", subset=([aux], slice(None))))
-
-    #Download button
-    buffer = io.BytesIO()
-    with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-        calculations_table.to_excel(writer, sheet_name='Sheet1', index=False)
-        download2 = st.download_button(label="Download data as Excel",data=buffer,file_name='Calculations Table.xlsx',mime='application/vnd.ms-excel')
-
 
 #Main Loop
 submit_button_check = 0
