d03a0198ce84cd6bebf827d915da04349873e409
SarahQuoos <sarahquoos@alunos.utfpr.edu.br>
2025-05-08 19:58:32 -0300

    Update Diametro_Economico.py

--- Diametro_Economico.py
+++ Diametro_Economico.py
@@ -148,14 +148,6 @@ def Main():
     complete_calculations_table = pd.DataFrame(complete_data_table)
 
     #Download button for Database
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    complete_calculations_table.to_excel(writer, index=False, sheet_name='Sheet1')
-    workbook = writer.book
-    worksheet = writer.sheets['Sheet1']
-    format1 = workbook.add_format({'num_format': '0.00'}) 
-    worksheet.set_column('A:A', None, format1)  
-    writer.save()
-    processed_data = output.getvalue()
     st.download_button(label='📥 Download Current Result',
                                 data=processed_data,
                                 file_name= 'df_test.xlsx')
