dc62b167cd2cf1e2c68071299ba7640dfd09e5d5
SarahQuoos <sarahquoos@alunos.utfpr.edu.br>
2025-05-09 09:54:38 -0300

    Update Diametro_Economico.py

--- Diametro_Economico.py
+++ Diametro_Economico.py
@@ -146,20 +146,16 @@ def Main():
             .applymap(lambda _: "background-color: LightSkyBlue;", subset=([aux], slice(None))) \
             .set_table_styles([
                 {'selector': 'th', 'props': [('text-align', 'center')]},
-                {'selector': 'td', 'props': [('text-align', 'center')]}
-            ])
+                {'selector': 'td', 'props': [('text-align', 'center')]}])
         
-        # Converting to HTML
-        #html = styled_df.to_html()
-        #styled_html = f"""
-        #<div style="height:500px; overflow:auto">
-        #{html}
-        #</div>
-        #"""
-        #st.markdown(styled_html, unsafe_allow_html=True)
-
-        styled_html = f"""<div style="height:500px; overflow:auto">{html}</div>"""
-        st.markdown(styled_html, unsafe_allow_html=True)
+            # Converting to HTML
+            html = styled_df.to_html()
+            styled_html = f"""
+            <div style="height:500px; overflow:auto">
+            {html}
+            </div>
+            """
+            st.markdown(styled_html, unsafe_allow_html=True)
        
     #Download complete calculations dataframe
     complete_data_table = {'Diâmetro Nominal [mm]': nominal_diameter, 'Diâmetro Interno [mm]': inner_diameter,'Área': area, 'Velocidade[m/s]': speed, 
